5cf5de1e0275fdb40f8af49aed8844a34239fecd
sptres <shawnpark2397@gmail.com>
2024-10-09 22:29:07 -0700

    fix mongo db issue

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -8,26 +8,25 @@ interface MongooseConnection {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-let cached: MongooseConnection = ((global as any) || null).mongoose || {};
+let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) {
-    throw new Error('MONGODB_URL is not defined');
-  }
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: 'Image-Enhancer',
+      dbName: 'imaginify',
       bufferCommands: false,
     });
 
